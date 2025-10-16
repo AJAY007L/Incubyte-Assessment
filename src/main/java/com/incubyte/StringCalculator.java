5bba242cf870d0b26925aa65e191fc617491c33a
AJAY007L <60544908+AJAY007L@users.noreply.github.com>
2025-10-16 20:35:57 +0530

    Refactor: rename variables for clarity

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class StringCalculator {
-    public static int add(String numbers) {
-        if (numbers.isEmpty()) return 0;
-        return Arrays.stream(numbers.split(","))
+    public static int add(String input) {
+        if (input.isEmpty()) return 0;
+        String[] tokens=input.split(",");
+        return Arrays.stream(tokens)
                 .mapToInt(StringCalculator::parseSingle)
                 .sum();
     }
 
-    private static int parseSingle(String numbers) {
-        return Integer.parseInt(numbers);
+    private static int parseSingle(String number) {
+        return Integer.parseInt(number);
     }
 }
