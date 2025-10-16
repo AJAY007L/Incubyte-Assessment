e66fca39bb786b6855025b2588ebe4a6aa2e9335
AJAY007L <60544908+AJAY007L@users.noreply.github.com>
2025-10-16 21:52:03 +0530

    Implement custom delimiter parsing (Green)

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -1,14 +1,21 @@
 package com.incubyte;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class StringCalculator {
     public static int add(String input) {
         if (input.isEmpty()) return 0;
-        String normalized = normalizeDelimiters(input);
-        String[] tokens=normalized.split(",");
+        String delimiters = ",|\n";
+        String numbers = input;
+        if (input.startsWith("//")) {
+            int idx = input.indexOf("\n");
+            delimiters = Pattern.quote(input.substring(2, idx));
+            numbers = input.substring(idx + 1);
+        }
+        String[] tokens = numbers.split(delimiters);
         return Arrays.stream(tokens)
                 .mapToInt(StringCalculator::parseSingle)
                 .sum();
