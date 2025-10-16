f16ebc7f8a27083a2bd4a1a0e23ccbc4470d5382
AJAY007L <60544908+AJAY007L@users.noreply.github.com>
2025-10-16 19:25:33 +0530

    Add test: empty input returns 0 (Red)

---
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.incubyte;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StringCalculatorTest {
+
+    @Test
+    public void add_EmptyString_ReturnsZero() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+}
