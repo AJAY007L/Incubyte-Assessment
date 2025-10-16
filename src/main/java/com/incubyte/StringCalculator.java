40be209f1135c8be1ff93a43c69a70c3b02b8343
AJAY007L <60544908+AJAY007L@users.noreply.github.com>
2025-10-16 20:31:12 +0530

    Implement sum of comma-separated numbers (Green)

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -1,11 +1,15 @@
 package com.incubyte;
 
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class StringCalculator {
     public static int add(String numbers) {
         if (numbers.isEmpty()) return 0;
-        return parseSingle(numbers);
+        return Arrays.stream(numbers.split(","))
+                .mapToInt(StringCalculator::parseSingle)
+                .sum();
     }
 
     private static int parseSingle(String numbers) {
