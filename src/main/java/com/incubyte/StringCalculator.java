37d8ba63c20e11b7231bb209ceb2d222cfa72838
AJAY007L <60544908+AJAY007L@users.noreply.github.com>
2025-10-16 19:28:31 +0530

    Implement add("") → 0 (Green)

---
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -0,0 +1,10 @@
+package com.incubyte;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class StringCalculator {
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) return 0;
+        throw new UnsupportedOperationException();
+    }
+}
